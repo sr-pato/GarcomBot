61b9d7757b209bfd93a1a403ce5f6de0fb931467
Sr_Yuu <oie.eu.sou.um@gmail.com>
2022-05-08 16:00:08 -0300

    Inicial

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import os
+import shutil
+from bs4 import BeautifulSoup as bs
+import feedparser as rssparser
+import yt_dlp
+import m3u8
+import json
+import getpass
+import telebot
+
